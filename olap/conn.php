2426cc43f0aae75e87fb2d91e8afc0e706106630
Yuli Setyo Budi <yulisetyo@kemenkeu.go.id>
2024-04-27 05:53:14 +0700

    perbaikan koneksi

--- olap/conn.php
+++ olap/conn.php
@@ -1,26 +1,4 @@
 <?php
-/*
-$host = "10.242.77.111"; //letak db oracle
-$port = "1521"; //port default oracle
-$tns = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))) (CONNECT_DATA=(SERVICE_NAME=SIKPDEV)))";
-       
-$db_user = "USERKUR";
-$db_pass = "kurmikro";
-
-try
-   {
-       $conn = new PDO("oci:dbname=".$tns, $db_user, $db_pass);
-       if(!$conn)
-       {
-          throw new Exception("KONEKSI KE SERVER GAGAL");	
-       }
-   }
-catch(PDOException $e)
-   {
-       echo ($e->getMessage())."<br /> <br />";
-   }
-*/   
-   
 class Conn 
 {
     
@@ -31,18 +9,13 @@ class Conn
 	    $this->link = $this->koneksi();
     }
     
+	/* KONEKSI KE SERVER ORACLE */
     public function koneksi()
     {
-		//~ $host = "10.242.77.111"; //host db oracle
-		//~ $port = "3306"; //port default oracle
-		//~ $service = "SIKPDEV";
-		//~ $tns = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))) (CONNECT_DATA=(SERVICE_NAME=$service)))";
-		
-		$port = "1521"; //port default oracle
 		$host = "10.216.208.6"; //host db oracle
-		$service = "SIKPOLAP";
-		$tns = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))) (CONNECT_DATA=(SERVICE_NAME=$service)))";
-		
+		$port = "1521"; //port default oracle
+		$service = "SIKPOLAP"; //nama service
+		$tns = "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))) (CONNECT_DATA=(SERVICE_NAME=$service)))"; //tns koneksi
 		$db_user = "USERKUR";
 		$db_pass = "kurmikro";
 		
@@ -64,7 +37,6 @@ class Conn
 		
 		}
     }
-
 }
 
 ?>
